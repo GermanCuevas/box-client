c0a0c06fb75687e95afa86eab21f9c99b69525f8
Nicolás Faure <nfaure@insideone.cloud>
2023-11-13 21:32:25 -0300

    fix: fix the tailwind config to see change on commons folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import PackageDetails from '@/commons/PackageDetails'
+
 export default function Home() {
   return (
     <main>
       <h1>HOLA MUNDO!</h1>
+      <PackageDetails />
     </main>
   )
 }
