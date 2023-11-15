916fa8dc2a7a637eab8daad2d7431f0a1e98dcfe
German <german.1990.cuevas@gmail.com>
2023-11-15 19:21:10 -0300

    fix: fix in header, change image/next to svg from function Icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Header from '@/components/Header'
+
 export default function Home() {
   return (
     <main className="flex flex-col items-center">
-      <h1>HOLA MUNDOOO!</h1>
+      <Header />
     </main>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,10 @@
-import Image from 'next/image'
+import { BoxTitle } from '@/commons/Icons'
 
 export default function Header() {
   return (
     <div className="w-full h-[70px]">
       <div className="py-[12px] px-[30px] flex justify-between items-center">
-        <Image src={'/img/box.svg'} width={80} height={30} alt="Logo box" />
+        <BoxTitle />
         {/* BOTON FAKE */}
         <button className="border rounded-xl text-xs h-4/5 py-0.5 px-2.5">CERRAR SESION</button>
       </div>
