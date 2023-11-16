d4ab263359147956004a7c77c7dd69c9c70c6884
Nicolás Faure <nfaure@insideone.cloud>
2023-11-16 19:12:20 -0300

    feat: init DetailsList common

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Input from '@/commons/Input'
+import LemmonButton from '@/commons/LemmonButton'
 
 export default function Home() {
   return (
     <main>
       <Input placeholder="Nombre" type="text" />
       <h1>HOLA MUNDOOO!</h1>
+      <LemmonButton height="30px" />
     </main>
   )
 }
