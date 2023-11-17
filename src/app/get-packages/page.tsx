94525326f025e95d1761d14b4ca033d186cbc3f3
Victoria Canclini <vico_canclini@hotmail.com>
2023-11-17 11:17:34 -0300

    feat: finished get packages

--- src/app/get-packages/page.tsx
+++ src/app/get-packages/page.tsx
@@ -10,33 +10,36 @@ import { ChevronDownBig } from '@/commons/Icons'
 
 export default function GetPackages() {
   return (
-    <div className=" bg-lightGreen ">
+    <div className=" bg-lightGreen h-screen">
       <Header />
       <div className="flex justify-center mb-4">
         <LemmonButton title={'obtener paquetes'} />
       </div>
-      <div className="ml-8 mr-8 bg-lightGreen max-h-[450px] text-sm">
-        <BoxTitle
-          titleBox={'Cuántos paquetes repartirás hoy?'}
-          titleBoxClasses={'ml-6'}
-          boxClasses={'ml-2 mr-2'}
-        />
-        <Address />
-        <Address />
-        <Address />
-        <Address />
-        <Address />
-        <Address />
-        <Address />
-        <Address />
-        <div className="bg-white flex justify-center items-center rounded-b-[15px] h-[3rem]">
+      <div className="ml-8 mr-8 bg-lightGreen text-xs flex flex-col justify-center items-center">
+        <div className="w-[100%]">
+          <BoxTitle
+            titleBox={'¿Cuántos paquetes repartirás hoy?'}
+            titleBoxClasses={'text-center'}
+            boxClasses={'justify-center h-10'}
+          />
+        </div>
+        <div className="w-[100%]">
+          <Address />
+          <Address />
+          <Address />
+          <Address />
+          <Address />
+          <Address />
+          <Address />
+        </div>
+        <div className="bg-white flex justify-center items-center rounded-b-[15px] h-[3rem] w-[100%]">
           <ChevronDownBig />
         </div>
       </div>
-      <div className="bg-lightGreen">
+      <div className="bg-lightGreen mt-4">
         <ButtomBottom
           titleButtom={'iniciar jornada'}
-          buttonClassName={'bg-darkGreen text-lemonGreen mb-6 mt-20'}
+          buttonClassName={'bg-darkGreen text-lemonGreen'}
         />
       </div>
     </div>
--- src/commons/Address.tsx
+++ src/commons/Address.tsx
@@ -9,20 +9,18 @@ export default function Address({ status, onClickButton }: address) {
   return (
     <div className="bg-white flex justify-start items-center border-b-[1px] border-darkGreen">
       <div
-        className={
-          'w-[30rem] h-[3.5rem] flex  items-center gap-x-4 ml-6 font-Roboto text-darkGreen'
-        }
+        className={'w-[30rem] h-[3.1rem] flex items-center gap-x-4 ml-6 font-Roboto text-darkGreen'}
         onClick={() => onClickButton?.()}
       >
         {status ? (
           <CheckOK
-            height="30px"
-            width="30px"
+            height="27px"
+            width="27px"
             color="#24424D"
             classNameCheck="bg-[#CEF169] rounded-full"
           />
         ) : (
-          <CheckOff classNameCheck="border-darkGreen border-[2px] w-[30px] h-[30px]" />
+          <CheckOff classNameCheck="border-darkGreen border-[1px] w-[27px] h-[27px]" />
         )}
         <h2>Amenabar 2356, CABA</h2>
       </div>
