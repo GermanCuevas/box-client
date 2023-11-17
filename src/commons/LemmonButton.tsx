71557682b1c1de14ede54cc1d20eba3c046dd73f
Nicolás Faure <nfaure@insideone.cloud>
2023-11-17 18:22:11 -0300

    fix: fix call of font

--- src/commons/LemmonButton.tsx
+++ src/commons/LemmonButton.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { saira } from '../../public/fonts/fonts'
+
 import { ArrowBack } from './Icons'
 
 interface LemmonButtonProps {
@@ -23,10 +25,10 @@ const LemmonButton: React.FC<LemmonButtonProps> = ({
       type="button"
       className={`bg-lemonGreen flex justify-between items-center rounded-[15px] pl-5 pr-5 max-w-[300px] ${height} ${width} ${tracking}`}
     >
-
-      <figcaption className="text-start flex flex-col justify-center  font-Saira h-full text-darkGreen ">
+      <figcaption
+        className={`text-start flex flex-col justify-center ${saira.className} antialiased h-full text-darkGreen`}
+      >
         <h3 className={'font-bold leading-4 tracking-[.5px] text-start text-lg uppercase'}>
-
           {title}
         </h3>
         {subtitle && <h5 className="lowercase tracking-wide text-xs font-thin">{subtitle}</h5>}
