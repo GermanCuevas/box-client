309a382393078d3828b66b82c5dc0ccb0a84f337
IvoDev28 <ivanlucana6@gmail.com>
2023-11-17 11:18:05 -0300

    feat: register-finalized

--- src/commons/LemmonButton.tsx
+++ src/commons/LemmonButton.tsx
@@ -31,7 +31,7 @@ const LemmonButton: React.FC<LemmonButtonProps> = ({
       <figcaption
         className={`text-start flex flex-col justify-center  ${saira.className} antialiased h-full text-darkGreen `}
       >
-        <h3 className={'font-bold leading-4 tracking-[1.5px] text-start text-lg uppercase'}>
+        <h3 className={'font-bold leading-4 tracking-[1.5px] text-start text-lg uppercase '}>
           {title}
         </h3>
         {subtitle && <h5 className="lowercase tracking-wide text-xs font-thin">{subtitle}</h5>}
