340fddf6e18f1b84d28cb27118e46a709fe98014
Nicolás Faure <nfaure@insideone.cloud>
2023-11-16 22:35:18 -0300

    feat: commot package details
    n

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
-import Input from '@/commons/Input'
-import LemmonButton from '@/commons/LemmonButton'
-
 export default function Home() {
   return (
     <main>
-      <Input placeholder="Nombre" type="text" />
       <h1>HOLA MUNDOOO!</h1>
-      <LemmonButton height="30px" />
     </main>
   )
 }
