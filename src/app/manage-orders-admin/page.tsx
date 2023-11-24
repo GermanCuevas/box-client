7bd58915bb6e396c3363bba931f860ae52b3ee4c
flormartinez92 <martinez.mf.92@gmail.com>
2023-11-24 17:53:21 -0300

    feat(common: datedetail): create dateDetail common

---
+++ src/app/manage-orders-admin/page.tsx
@@ -0,0 +1,10 @@
+import DateDetail from '@/commons/DateDetail';
+import React from 'react';
+
+export default function ManageOrdersAdmin() {
+  return (
+    <div className="bg-lightGreen h-screen">
+      <DateDetail />
+    </div>
+  );
+}
